a7cccc150ce1fdb695d8d3107912f07fcf478523
reececrowther <reececrowther@hotmail.co.uk>
2021-06-08 16:58:02 +0100

    Removed file

--- node_modules/vite-plugin-string/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Plugin } from 'vite';
-import { FilterPattern } from '@rollup/pluginutils';
-export interface Options {
-    include?: FilterPattern;
-    exclude?: FilterPattern;
-    compress?: boolean | ((code: string) => string);
-}
-export default function (userOptions?: Options): Plugin;
